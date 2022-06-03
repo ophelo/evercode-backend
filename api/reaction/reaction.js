38b90de0122a861fe3a2712eb2c288b2c5ebbac7
EdoardoCisco <ciscoedoardo@gmail.com>
2022-06-03 22:30:44 +0200

    last changes before test

--- api/reaction/reaction.js
+++ api/reaction/reaction.js
@@ -1,20 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const { Project } = require('../project/model');
 
 const reactionSchema = new mongoose.Schema({
     reacter: {  type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    reaction: { type: String, enum: [ 'UpVote', 'DownVote' ], required: true },
+    reactionVal: { type: String, enum: [ 'UpVote', 'DownVote' ], required: true },
     projectReacted: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true }
 })
 
-reactionSchema.pre('save', async function(next){
-/*await Project.updateOne({_id: this.projectReacted},{
-    if(this.reaction === ){
+reactionSchema.methods.updateProject = async function(_idProject){
+switch(this.reactionVal){
+    case 'UpVote': 
+        await Project.updateOne({ _id: _idProject }, { $inc: { "meta.UpVote": 1 }});
+        break;
+    case 'DownVote': 
+        await Project.updateOne({ _id: _idProject }, { $inc: { "meta.DownVote": 1 }});
+        break;
+    default:
+      return res.status(400).json({"error": "bad request!"}) 
+}
+}
 
-    }else if( )
-});
-next();*/
-})
+reactionSchema.methods.updateOnDeleteProject = async function(_idProject){
+switch(this.reactionVal){
+    case 'UpVote': 
+        await Project.updateOne({ _id: _idProject }, { $inc: { "meta.UpVote": -1 }});
+        break;
+    case 'DownVote': 
+        await Project.updateOne({ _id: _idProject }, { $inc: { "meta.DownVote": -1 }});
+        break;
+    default:
+      return res.status(400).json({"error": "bad request!"}) 
+}
+}
+
 const Reaction = mongoose.model('Reaction', reactionSchema)
 
 module.exports = Reaction
--- api/reaction/routes.js
+++ api/reaction/routes.js
@@ -4,11 +4,41 @@ const escapeStringRegexp = import('escape-string-regexp')
 const { User, Profile } = require('../user/model')
 const { param } = require('../project/routes')
 const { find } = require('underscore')
+const Reaction = require('./reaction')
 const commentRoutes = express.Router()
 
 //new reaction
+reactionRoutes.post(':_id/addReaction', async (req, res) =>{
+  const user = await User.findOne({
+    email: req.auth['https://evercode.com/email']
+  })
+  try{
+const reaction = await Reaction.find({ reacter: user._id, projectReacted: req.params._id })
+if(reaction != null ){ return res.status(400).json({reaction}) }
+    else{ 
+      Reaction.create({ reacter: user._id, projectReacted: req.params._id })
+      .then( async react => {
+          react.updateProject(req.params._id)
+      } )
+    }
+  }catch(err){return res.status(500).json({ message: err.message })}
+})
 
-//delete reaction
+reactionRoutes.delete(':_id/delete/:idReaction', async (req, res) =>{
+  const user = await User.findOne({
+    email: req.auth['https://evercode.com/email']
+  })
+if (user._id.toString() === req.body.reacter.toString()){ 
+ try{
+  await Reaction.delete({ _id: req.params.idReaction })
+  .then(async react => {
+    react.updateOnDelateProject(req.params._id)
+  })
+  return res.json({ message: 'Deleted reaction' })
+  }catch(err){
+   return res.status(500).json({ message: err.message })
+  }}else{ return res.status(403).json({ message: 'Forbidden' })}
+})
 
 
 module.exports = reactionRoutes
