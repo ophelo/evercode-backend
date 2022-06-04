6a07913227d8e34cffd078eb0b93b361a1cd1850
Davide Frageri <davide.frag@gmail.com>
2022-06-04 16:11:16 +0200

    handle error in middleware

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -2,5 +2,7 @@ const { User } = require('../user/model')
 
 exports.getUser = async (req, res, next) => {
   req.user = await User.findOne({ email: req.auth['https://evercode.com/email'] })
+    .catch(err => { return res.status(500).json({error: "internal error!"})})
+  if (!req.user) return res.status(404).json({error: "no user found!"});
   next()
 }
